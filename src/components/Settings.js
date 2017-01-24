be0be56c3597947993803d3a1bd3888f93fb7609
Gabi Procell <gprocell927@gmail.com>
2017-01-24 11:02:12 -0700

    Sets up routing for jokes, favorites, settings, and root

---
+++ src/components/Settings.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Settings extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Settings</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Settings;
