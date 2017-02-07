d1524ed26820063c3d3f92c288f37c918ef92d63
Gabi Procell <gprocell927@gmail.com>
2017-02-07 09:39:36 -0700

    creates FooterText component. Settings page is now devoid of unwanted app elements

--- src/components/FooterText/FooterText.js
+++ src/components/FooterText/FooterText.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
-const FavoritesButton = (props) => {
+const FooterText = (props) => {
   const { path } = props
   if (path === '/settings'){
     return null
   } else {
     return (
       <div>
-        <button children="Favorites" />
         <span>Click Get Jokes!</span>
       </div>
     )
   }
 }
-export default FavoritesButton
+export default FooterText
