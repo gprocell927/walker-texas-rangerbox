fe8cca36ce0caacf1048bc808e88149d6f65bd84
Gabi Procell <gprocell927@gmail.com>
2017-02-07 09:33:31 -0700

    creates FavoritesButton component

---
+++ src/components/FavoritesButton/FavoritesButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FavoritesButton = () => {
+  return (
+    <div>
+      <button children="Favorites" />
+      <span>Click Get Jokes!</span>
+    </div>
+  )
+}
+
+export default FavoritesButton
