e7f2b2f706255a0026d0ff349fd646a819110efe
Gabi Procell <gprocell927@gmail.com>
2017-02-09 10:34:14 -0700

    wip : styling to match comp

--- src/App/App.js
+++ src/App/App.js
@@ -5,15 +5,22 @@ import RandomJokes from '../components/RandomJokes/RandomJokes'
 import JokeRequest from '../components/JokeRequest/JokeRequest'
 import FavoritesButton from '../components/FavoritesButton/FavoritesButton'
 import FooterText from '../components/FooterText/FooterText'
-import './App.css';
+import './App.css'
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
       numJokes: 0,
-      reqJokes: []
+      reqJokes: [],
+      jokes: [],
+      settings: {
+        firstName: '',
+        lastName: '',
+        censorJokes: true
+      }
     }
+    this.setName = this.setName.bind(this)
   } // end of constructor
 
 
@@ -34,19 +41,47 @@ class App extends Component {
    } // end of handleChange
       //sets the requested number of jokes to numJokes state
 
+  componentDidMount(){
+    axios
+      .get(`http://api.icndb.com/jokes/random?firstName=${this.state.settings.firstName}&amp;lastName=${this.state.settings.lastName}?escape=javascript`)
+      .then(res => res.data.value.joke)
+      .then(jokes => this.setState({ jokes }))
+    } // end of handleRandomJoke
+
+  setName(name) {
+    const { settings } = this.state
+    const firstName = name.split(' ')[0]
+    const lastName = name.split(' ')[1]
+    this.setState({
+      ...this.state,
+      settings: {
+        ...settings,
+        firstName,
+        lastName,
+      }
+    })
+  }
+
 // use pathname in a conditional to render/hide
   render() {
     const Children = React.cloneElement(this.props.children, {
 		    numJokes: this.state.jokes,
         reqJokes: this.state.reqJokes,
+        jokes: this.state.jokes,
+        firstName: this.state.settings.firstName,
+        lastName: this.state.settings.lastName,
+        censorJokes: this.state.settings.censorJokes,
+        setName: this.setName
       }
     )
+    console.log(this.state.settings)
     return (
       <div>
         <Header
           path={this.props.location.pathname}
         />
-        <RandomJokes />
+        <RandomJokes
+          handleLoad={ () => this.handleRandomJoke() }/>
         <JokeRequest
           handleInputChange={ (e) => this.handleChange(e) }
           handleButtonClick={ (e) => this.handleClick(e) }
